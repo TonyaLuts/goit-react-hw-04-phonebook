058ae0ef71a4e631235f4582724f205cec15db3a
TonyaLuts <131724958+TonyaLuts@users.noreply.github.com>
2023-12-03 19:58:43 +0000

    add getInitialContacts

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -8,19 +8,21 @@ import { Container } from './App.styled.js';
 
 const keyLS = 'contacts-items';
 
+const getInitialContacts = () => {
+  const savedContacts = window.localStorage.getItem(keyLS);
+  if (savedContacts !== null) {
+    return JSON.parse(savedContacts);
+  }
+  return [
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ];
+};
+
 export const App = () => {
-  const [contacts, setContacts] = useState(() => {
-    const savedContacts = window.localStorage.getItem(keyLS);
-    if (savedContacts !== null) {
-      return JSON.parse(savedContacts);
-    }
-    return [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ];
-  });
+  const [contacts, setContacts] = useState(getInitialContacts);
   const [filter, setFilter] = useState('');
 
   useEffect(() => {
